8748e6edff21573fa786f35a82096339f632af48
masterj <ronchaos@gmail.com>
2018-02-14 10:49:14 +0900

    * test_new.sh update   add "bundle update" command   this command is update for Gemfile.lock file

--- test_new.sh
+++ test_new.sh
@@ -1,4 +1,5 @@
 ##  imac Darwin  not working, so must this command using 
 ## 17.4.0 Darwin Kernel Version 17.4.0: nu-4570.41.2~1/RELEASE_X86_64 x86_64
+bundle update
 bundle exec jekyll build
 bundle exec jekyll serve
